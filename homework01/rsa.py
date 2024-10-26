6fa08a36092c82ff684ce34ea37b2ef45c2cffb7
Sofia Popkova <sofiaprezident@gmail.com>
2024-10-27 02:08:13 +0300

    Реализована функция is_prime(n)

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,7 +12,15 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
+    if n <= 1:
+        return False
+    if n % 2 == 0:
+        return False
+    if n == 2:
+        return True
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
     pass
 
 
