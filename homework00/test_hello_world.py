36f0c5499f0dd05f26f34c0e8323802aebd9abe8
Sofia Popkova <sofiaprezident@gmail.com>
2024-11-14 15:17:02 +0300

    исправлены недочеты в test.hello-world

--- homework00/test_hello_world.py
+++ homework00/test_hello_world.py
@@ -8,17 +8,3 @@ class HelloTestCase(unittest.TestCase):
         m = "message"
         self.assertEqual(m, hello_world.text())
         
-ciphertext = ""
-    for symbol in plaintext:
-        if symbol.isalpha(): # Если символ - это буква
-          if symbol >= "a" and symbol <= "z":
-              ciphertext += chr(((ord(symbol) + 2 - ord('a')) % 26) + ord('a'))
-          else:
-              ciphertext += symbol
-        elif symbol.isupper():
-          if symbol.isupper():
-            if symbol >= "A" and symbol <= "Z":
-                ciphertext += chr(((ord(symbol) + 2 - ord('A')) % 26) + ord('A'))
-            else:
-                ciphertext += symbol
-    return ciphertext
